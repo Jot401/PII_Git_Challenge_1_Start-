0e641965ff7461e5faf21e85ccef9875f23d1985
FrancoManfredi <franco.manfredi@correo.ucu.edu.uy>
2024-08-12 19:20:39 -0300

    Borrado de otras features

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,26 +1,5 @@
-﻿Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2));
-Console.WriteLine(Addition.Add(7, 2));
-Console.WriteLine(Substraction.Substract(10, 1));
+﻿Console.WriteLine(Addition.Add(7, 2));
 
-
-// Esta clase implementa la operación Division
-public class Division
-{
-    public static double Divide(int a, int b)
-    {
-        return (double)a / b;
-    }
-}
-
-// Esta clase implementa la operación Multiplicación
-public class Multiplication
-{
-    public static int Multiply(int a, int b)
-    {
-        return a * b;
-    }
-}
 public class Addition
 {
     public static int Add(int a, int b)
